040af4d07ba9bf7024528c21178373a03a5d79b4
charlesliucn <charleyliu.cn@gmail.com>
2019-06-30 18:19:27 +0800

    [clean]

--- egs/babel/s5d/run-0-results.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for x in exp_bnf/{tri,sgmm}*/decode*; do [ -d $x ] && echo $x | grep "${1:-.*}" >/dev/null && grep WER $x/wer_* 2>/dev/null | utils/best_wer.sh; done
-for x in exp_bnf/{tri,sgmm}*/decode*; do [ -d $x ] && echo $x | grep "${1:-.*}" >/dev/null && grep Sum $x/score_*/*.sys 2>/dev/null | utils/best_wer.sh; done
-exit 0
